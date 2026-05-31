5f425a903bf8fcce8d2e4fc76f3e94976be24180
sohamghosh1762-max <sohamghosh1762@gmail.com>
2026-05-31 14:50:48 +0530

    Fixed main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1>Sentient Sentinel</h1>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
